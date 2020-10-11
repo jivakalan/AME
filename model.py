b917f742e82b37028cacd76becb35a6cc34d3140
Jiva <kalan.jiva@gmail.com>
2020-10-11 10:45:12 -0400

    ready for delivery

--- model.py
+++ model.py
@@ -5,106 +5,17 @@ Created on Sat Oct 10 09:56:44 2020
 @author: jkalan
 """
 
+
+#############
+## Imports ##
+#############
 import pandas as pd
-import matplotlib.pyplot as plt
-%matplotlib inline
-import seaborn as sns
 import numpy as np
-import statistics as st
-# =============================================================================
-# retrieved data and column info from:
-# https://archive.ics.uci.edu/ml/datasets/census+income
-# =============================================================================
-
-column_names = [ 'age', 'workclass', 'fnlwgt', 'education', 'education_num', 'marital_status','occupation'
-                ,'relationship','race','sex','capital_gain','capital_loss', 'hrs_per_week', 'native_country', 'income']
-train_set = pd.read_csv('adult-training.csv', names =column_names)
-
-test_set = pd.read_csv('adult-test.csv',skiprows=1,names = column_names)
-
-# =============================================================================
-# 
-# Make a quick statistic based and univariate audit of the different columns’ 
-# content and produce the results in visual / graphic format.
-#
-# This audit should describe the variable distribution, the % of missing values
-# , the extreme values, and so on.
-# 
-# =============================================================================
-
-##missing values --appear to be none
-train_set.info()
-test_set.info()
-##remove whitespaces
-train_set.replace(' ?',np.nan,inplace=True)
-test_set.replace(' ?',np.nan,inplace=True)
-
-descr_table = train_set.describe()
-a= test_set.describe()
-##counts
-for name in column_names:
-    print(train_set[name].value_counts())
-##unique values
-for name in column_names:
-    print(name, train_set[name].unique())
- 
-    
-#############################
-##visual audit of columns###
-############################
-
-##bar charts
-    ###fix labels!!!!!! 
-fig,ax=plt.subplots(4,2,figsize=(15,15))
-sns.countplot(x=train_set["workclass"],ax=ax[0,0])
-sns.countplot(x=train_set["education"], ax=ax[0,1])
-sns.countplot(x=train_set["marital_status"], ax=ax[1,0])
-sns.countplot(x=train_set["relationship"],ax=ax[1,1])
-sns.countplot(x=train_set["sex"], ax=ax[2,0])
-sns.countplot(x=train_set["occupation"], ax=ax[3,0])
-sns.countplot(x=train_set["race"], ax=ax[3,1])
-
-fig.show()
-
-##histograms
-fig,ax=plt.subplots(2,3,figsize=(15,5))
-train_set.hist("age",ax=ax[0,0])
-train_set.hist("education_num", ax=ax[0,1])
-train_set.hist("capital_gain", ax=ax[0,2])
-train_set.hist("hrs_per_week", ax=ax[1,0])
-train_set.hist("fnlwgt", ax=ax[1,1])
-train_set.hist("capital_loss", ax=ax[1,2])
-
-#visualize extreme values
-fig,ax=plt.subplots(2,3,figsize=(15,5))
-train_set.boxplot("age", ax=ax[0,0])
-train_set.boxplot("education_num", ax=ax[0,1])
-train_set.boxplot("capital_gain", ax=ax[0,2])
-train_set.boxplot("fnlwgt", ax=ax[1,1])
-train_set.boxplot("capital_loss", ax=ax[1,2])
-train_set.boxplot("hrs_per_week",ax=ax[1,0])
-
-cols_to_filter =["age","education_num","capital_gain","hrs_per_week","fnlwgt","capital_loss"]
-
-#function to cap extreme values 
-def extreme_cap(df, num_std_dev):
-    df_capped = df
-    for col in descr_table:
-       # v = st.stdev(df[col])*num_std_dev
-        v = descr_table[col]["std"]*num_std_dev
-        hi = descr_table[col]["mean"]+v
-        lo = descr_table[col]["mean"]-v
-        df_capped[col]=df_capped[col].apply(lambda x: hi if x > hi else x)
-        df_capped[col]=df_capped[col].apply(lambda x: lo if x < lo else x)
-    return df_capped
-
-
-
-#cap values in train set
-train_set = extreme_cap(train_set, 1.5)
-
-##cap extreme values on test set 
-test_set = extreme_cap(test_set, 1.5)
+import xgboost as xgb
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import precision_score
 
 # =============================================================================
 # 
@@ -124,9 +35,59 @@ test_set = extreme_cap(test_set, 1.5)
 # =============================================================================
 
 
-import xgboost as xgb
-from sklearn.metrics import roc_auc_score
-from sklearn.preprocessing import OneHotEncoder
+
+
+# =============================================================================
+# retrieved data and column info from:
+# https://archive.ics.uci.edu/ml/datasets/census+income
+# =============================================================================
+
+column_names = [ 'age', 'workclass', 'fnlwgt', 'education', 'education_num', 'marital_status','occupation'
+                ,'relationship','race','sex','capital_gain','capital_loss', 'hrs_per_week', 'native_country', 'income']
+train_set = pd.read_csv('adult-training.csv', names =column_names)
+
+test_set = pd.read_csv('adult-test.csv',skiprows=1,names = column_names)
+
+##get some initial info
+train_set.info()
+test_set.info()
+##remove whitespaces and deal with ? values
+train_set.replace(' ?',np.nan,inplace=True)
+test_set.replace(' ?',np.nan,inplace=True)
+
+##descr table will have std dev and mean values for use in extreme_cap fn 
+descr_table = train_set.describe()
+a= test_set.describe()
+
+##counts/unique values
+for name in column_names:
+    print(train_set[name].value_counts())
+##unique values
+for name in column_names:
+    print(name, train_set[name].unique())
+ 
+#cols_to_filter =["age","education_num","capital_gain","hrs_per_week","fnlwgt","capital_loss"]
+
+#function to cap extreme values 
+def extreme_cap(df, num_std_dev):
+    df_capped = df
+    for col in descr_table:
+       # v = st.stdev(df[col])*num_std_dev
+        v = descr_table[col]["std"]*num_std_dev
+        hi = descr_table[col]["mean"]+v
+        lo = descr_table[col]["mean"]-v
+        df_capped[col]=df_capped[col].apply(lambda x: hi if x > hi else x)
+        df_capped[col]=df_capped[col].apply(lambda x: lo if x < lo else x)
+    return df_capped
+
+
+
+#cap values in train set
+train_set = extreme_cap(train_set, 1.5)
+##cap extreme values on test set 
+test_set = extreme_cap(test_set, 1.5)
+
+
 
 #####################
 ## Pre-processing ##
@@ -152,7 +113,7 @@ for name in col_cat:
 train_features.drop(col_cat, axis='columns',inplace=True)
  
 
-##one-hot-encod test features
+##one-hot-encoding test features
 for name in col_cat:
     name_df = pd.DataFrame(test_features[name])
     dum_df  = pd.get_dummies(name_df, prefix=[name+'_'] )
@@ -160,9 +121,9 @@ for name in col_cat:
     
 test_features.drop(col_cat, axis='columns',inplace=True)
 
-##add column native_country__ Holand-Netherlands
+##add column native_country__ Holand-Netherlands so that columns are same in test and train
 test_features["native_country__ Holand-Netherlands"]=0
-
+#sorting test and train features so order is identical
 test_features = test_features.reindex(sorted(test_features.columns), axis=1)
 train_features = train_features.reindex(sorted(train_features.columns), axis=1)
 
@@ -174,11 +135,10 @@ train_features = train_features.reindex(sorted(train_features.columns), axis=1)
 model = xgb.XGBClassifier( objective= 'binary:logistic'
                          , colsample_bytree= 0.3
                          , learning_Rate =.1
-                         , max_depth=3  
-                         , alpha=10  ##l1 regularization
+                         , max_depth=10
+                         , verbosity =0
                          )
 model.fit(train_features, train_target)
-print(model)
 preds =model.predict(test_features)
 
 
@@ -186,55 +146,58 @@ preds =model.predict(test_features)
 ## XGB Performance   ##
 #######################
 
-roc = roc_auc_score(y_true=test_target, y_score=preds)
+xgb_roc = roc_auc_score(y_true=test_target, y_score=preds)
+print('AUC:', round(xgb_roc*100,2))
+
+
+
+############################
+## Model 2 - RandomForest ##
+############################
+
+model_rf = RandomForestClassifier(n_estimators= 100 ##num trees
+                                 , max_depth=10     ##num levels in forest
+                                 , random_state=0   
+                                 , min_samples_splitint =2  #min num samples to split on
+                                 , min_samples_leaf = 2    #min samples at each node
+                                 )
+
+model_rf.fit(train_features, train_target)
+
+rf_preds = model_rf.predict(test_features)
+
+roc = roc_auc_score(y_true=test_target, y_score=rf_preds)
 print('AUC:', round(roc*100,2))
-
-#################
-## XGB Tuning ##
-################
-
-##grid search to tune hyperparameters
-
-
-
+precision_score(y_true=test_target, y_pred= rf_preds, average='weighted')
 
 #############################
-## Model 2: Neural Network ##
-#############################
-
-from keras import Sequential
-from keras.layers import Dense
-
-model = Sequential()
-model.add(Dense(12, input_dim=105, activation='relu'))
-model.add(Dense(8, activation='relu'))
-model.add(Dense(1, activation='sigmoid'))
-
-print(model)
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-model.fit(train_features, train_target, epochs=25, batch_size=10)
+##  Hyperparameter Tuning ##
+############################
 
 
-nn_preds = model.predict(test_features)
-rounded = [round(x[0]) for x in nn_preds]
+grid = { 'n_estimators': list(range(75,300, 25))
+       , 'max_depth': list(range(5,50, 5))
+       , 'min_samples_split': [2,4,6]
+       , 'min_samples_leaf': [1,2,3,4]
+       , 'bootstrap': [True,False]}
 
-#######################
-## NN Performance   ##
-#######################
+rf_tuning = RandomizedSearchCV(  estimator=model_rf
+                               , param_distributions= grid
+                               , n_iter = 10
+                               , scoring='roc_auc'
+                               , cv = 5
+                               , verbose=2
+                               , random_state=0
+                               , return_train_score=True)
 
-_, accuracy = model.evaluate(X, y)
-print('Accuracy: %.2f' % (accuracy*100))
+## this will take a few minutes
+rf_tuning.fit(train_features, train_target)
 
+best_params_rf= rf_tuning.best_params_
+best_model = rf_tuning.best_estimator_
+best_model.fit(train_features,train_target)
+best_preds = best_model.predict(test_features)
 
-roc = roc_auc_score(y_true=test_target, y_score=rounded)
-print('AUC:', round(roc*100,2))
-
-
-#################
-## NN Tuning ##
-################
-
-##grid search to tune hyperparameters
-
-
+best_roc = roc_auc_score(y_true=test_target, y_score=best_preds)
+print('Tuned AUC:', round(best_roc*100,2))
+print('Initial AUC:', round(roc*100,2))
---
+++ viz.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 11 10:37:11 2020
+
+@author: jkalan
+"""
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+# =============================================================================
+# retrieved data and column info from:
+# https://archive.ics.uci.edu/ml/datasets/census+income
+# =============================================================================
+
+column_names = [ 'age', 'workclass', 'fnlwgt', 'education', 'education_num', 'marital_status','occupation'
+                ,'relationship','race','sex','capital_gain','capital_loss', 'hrs_per_week', 'native_country', 'income']
+train_set = pd.read_csv('adult-training.csv', names =column_names)
+
+test_set = pd.read_csv('adult-test.csv',skiprows=1,names = column_names)
+
+# =============================================================================
+# 
+# Make a quick statistic based and univariate audit of the different columns’ 
+# content and produce the results in visual / graphic format.
+#
+# This audit should describe the variable distribution, the % of missing values
+# , the extreme values, and so on.
+# 
+# =============================================================================
+
+##missing values --appear to be none
+train_set.info()
+test_set.info()
+##remove whitespaces
+train_set.replace(' ?',np.nan,inplace=True)
+test_set.replace(' ?',np.nan,inplace=True)
+
+descr_table = train_set.describe()
+a= test_set.describe()
+##counts
+for name in column_names:
+    print(train_set[name].value_counts())
+##unique values
+for name in column_names:
+    print(name, train_set[name].unique())
+ 
+    
+#############################
+##visual audit of columns###
+############################
+
+##bar charts
+    ###fix labels!!!!!! 
+fig,ax=plt.subplots(4,2,figsize=(15,15))
+sns.countplot(x=train_set["workclass"],ax=ax[0,0])
+sns.countplot(x=train_set["education"], ax=ax[0,1])
+sns.countplot(x=train_set["marital_status"], ax=ax[1,0])
+sns.countplot(x=train_set["relationship"],ax=ax[1,1])
+sns.countplot(x=train_set["sex"], ax=ax[2,0])
+sns.countplot(x=train_set["occupation"], ax=ax[3,0])
+sns.countplot(x=train_set["race"], ax=ax[3,1])
+fig.show()
+
+##histograms
+fig,ax=plt.subplots(2,3,figsize=(15,5))
+train_set.hist("age",ax=ax[0,0])
+train_set.hist("education_num", ax=ax[0,1])
+train_set.hist("capital_gain", ax=ax[0,2])
+train_set.hist("hrs_per_week", ax=ax[1,0])
+train_set.hist("fnlwgt", ax=ax[1,1])
+train_set.hist("capital_loss", ax=ax[1,2])
+
+#visualize extreme values
+fig,ax=plt.subplots(2,3,figsize=(15,5))
+train_set.boxplot("age", ax=ax[0,0])
+train_set.boxplot("education_num", ax=ax[0,1])
+train_set.boxplot("capital_gain", ax=ax[0,2])
+train_set.boxplot("fnlwgt", ax=ax[1,1])
+train_set.boxplot("capital_loss", ax=ax[1,2])
+train_set.boxplot("hrs_per_week",ax=ax[1,0])
+
